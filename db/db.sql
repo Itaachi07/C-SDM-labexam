25d80905605280c10dbe21450a2c8ca18867d401
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-06-25 14:17:20 +0530

    project created and db.sql created

---
+++ db/db.sql
@@ -0,0 +1,9 @@
+
+create table Employee (
+    emp_id int , 
+    emp_Name varchar(50), 
+    emp_Address varchar(50) ,
+    emp_Email varchar(50),
+    emp_MobileNo varchar(50),
+    emp_DOB DATE, 
+    emp_JoiningDate DATE)

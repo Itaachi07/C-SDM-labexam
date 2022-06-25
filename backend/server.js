8fb49c303ae8a4f0e8c18e046c25c8d58a9f1114
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-06-25 14:30:05 +0530

    server.js created

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express
+
+app.use(cors('*'))
+app.use(express.json())
+
+const employeeRouter = require('./routes/employee')
+app.use('/employee',employeeRoute);
+
+app.listen(4000,'0.0.0.0',()=>{
+    console.log('server started on port 4000')
+})
